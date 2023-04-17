b1be9c2f0e276c528d02fadc481712ea05cda291
Zephtos <damian.hoogsteder@indicia.nl>
2023-04-17 16:04:44 +0200

    missed migrations

---
+++ Forfeit15.Postgres/Forfeit15.Postgres/Migrations/20230417105452_InitialCommit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forfeit15.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PatchNotes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatchNotes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InfoNodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatchNoteId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ImageUrl = table.Column<string>(type: "text", nullable: true),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    NodeType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfoNodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InfoNodes_PatchNotes_Id",
+                        column: x => x.Id,
+                        principalTable: "PatchNotes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InfoNodes");
+
+            migrationBuilder.DropTable(
+                name: "PatchNotes");
+        }
+    }
+}
